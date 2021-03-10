a319b7c1b7bbc9abd184a5862155f7ad431ee45a
Darryl Mangibin <darrylmangibin@gmail.com>
2021-03-10 20:55:06 +0800

    Intial setup for simple app

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
+interface AppProps {
+  color?: string
+}
+
+class App extends React.Component<AppProps> {
+  state = {
+    counter: 0
+  }
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 })
+  }
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 })
+  }
+
   render() {
     return (
       <div>
-        React Typescript
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
       </div>
     )
   }
 }
 
 ReactDOM.render(
-  <App />,
+  <App color="red" />,
   document.querySelector('#root')
 )
